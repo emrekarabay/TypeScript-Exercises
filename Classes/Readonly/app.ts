3eb78e7579fbaf6d8324c171ee0b1f3ac34d13a6
Süleyman Emre Karabay <karabayemre10@gmail.com>
2023-02-15 14:36:51 +0300

    Classes\Readonly eklendi

---
+++ Classes/Readonly/app.ts
@@ -0,0 +1,21 @@
+class Person {
+    readonly id: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(id: number, firstName: string, lastName: string){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+let user = new Person(45,"Emre","Karabay");
+console.log(user);
+
+// user.id = 5 // error
+console.log(user.getFullName());
