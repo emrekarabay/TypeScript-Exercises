745f9b2caa0f0eb50e449546366df7d2065b6221
Süleyman Emre Karabay <karabayemre10@gmail.com>
2023-02-15 08:49:24 +0300

    DataTypes\Object eklendi

---
+++ DataTypes/Object/app.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var instrustor;
+instrustor = {
+    firstName: "Emre",
+    lastName: "Karabay",
+    age: 23,
+    jobTitle: "Software Developer"
+};
+console.log(instrustor.jobTitle);
+var person = {
+    firstName: "Emre2",
+    lastName: "Karabay2",
+    age: 235,
+    jobTitle: "Software Developer2"
+};
+console.log(person.jobTitle);
