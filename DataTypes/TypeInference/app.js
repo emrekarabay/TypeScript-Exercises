494c7204e0dc845b17ae3ae8617e9b9936001676
Süleyman Emre Karabay <karabayemre10@gmail.com>
2023-02-15 11:19:28 +0300

    DataTypes\TypeInference eklendi

---
+++ DataTypes/TypeInference/app.js
@@ -0,0 +1,5 @@
+var counter = 0;
+console.log(typeof (counter));
+var counter2 = 0;
+console.log(typeof (counter2));
+// atamadığımız halde tipinin tutulmasına denir.
---
+++ DataTypes/TypeInference/app.ts
@@ -0,0 +1,9 @@
+let counter = 0;
+
+console.log(typeof(counter));
+
+let counter2: number = 0;
+
+console.log(typeof(counter2));
+
+// atamadığımız halde tipinin tutulmasına denir.
