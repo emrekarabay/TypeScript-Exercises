284a2822db26dc5e90fa85c1c3d7725a8cdb2c1a
Süleyman Emre Karabay <karabayemre10@gmail.com>
2023-02-15 09:10:16 +0300

    DataTypes\Any eklendi

---
+++ DataTypes/Any/app.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var value = "Emre";
+value = true;
+value = {
+    firstName: "Süleyman",
+    lastName: "Karabay"
+};
+console.log(value);
+var arr = ["Emre", 23, false];
+console.log(arr);
---
+++ DataTypes/Any/app.ts
@@ -0,0 +1,12 @@
+let value : any = "Emre";
+value = true
+value = {
+    firstName: "Süleyman",
+    lastName: "Karabay"
+}
+console.log(value);
+
+let arr: any[] = ["Emre",23,false];
+console.log(arr);
+
+export {};
