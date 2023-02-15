292ae6bd3c4a6c1fcece24d343b462841e3d0278
Süleyman Emre Karabay <karabayemre10@gmail.com>
2023-02-15 08:34:01 +0300

    DataTypes\Tuple eklendi

---
+++ DataTypes/Tuple/app.js
@@ -0,0 +1,5 @@
+var instructor = [1, "Emre"];
+console.log(instructor);
+var employee;
+employee = [[1, "Emre"], [2, "Ali"], [5, "Süleyman"]];
+console.log(employee);
---
+++ DataTypes/Tuple/app.ts
@@ -0,0 +1,7 @@
+let instructor: [number,string] = [1,"Emre"];
+
+console.log(instructor);
+
+let employee: [number,string][];
+employee = [[1,"Emre"],[2,"Ali"],[5,"Süleyman"]];
+console.log(employee);
